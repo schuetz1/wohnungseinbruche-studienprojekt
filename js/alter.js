3555b4a394efc0d09f5d5285ed9be82ca2dfce5a
luizie <LuisaZiegler@gmx.de>
2017-01-25 20:02:00 +0100

    alter legenden angepasst

--- js/alter.js
+++ js/alter.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 
         title: {
             text: 'Sortiert nach Deutsche und Nicht-Deutsche Täter',
+            fontFamily: 'Lato',
             style: {
                 color: '#fff'
             }
@@ -18,16 +19,16 @@ $(document).ready(function () {
             labels: {
                 format: '{value}',
                 style: {
-                    color: '#000'
+                    color: '#fff'
                 }
             },
             categories: [8, 12, 14, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76],
             tickInterval: null,
 
             title: {
-                text: 'Alter',
+                text: '',
                 style: {
-                    color: '#000'
+                    color: '#fff'
                 }
             }
         },],
@@ -36,7 +37,7 @@ $(document).ready(function () {
             labels: {
                 format: '{value}',
                 style: {
-                    color: '#000'
+                    color: '#fff'
                 }
             },
             tickInterval: null,
@@ -44,16 +45,16 @@ $(document).ready(function () {
             title: {
                 text: 'Anzahl der Einbrüche',
                 style: {
-                    color: '#000'
+                    color: '#fff'
                 }
             }
         },],
 
         tooltip: {
+            backgroundColor:'#fff',
+            borderWidth:0,
             formatter: function () {
-                return '<b>' + this.x + '</b><br/>' +
-                    this.series.name + ': ' + this.y + '<br/>' +
-                    'Total: ' + this.point.stackTotal;
+                return '<b>' + this.series.name + ' Täter'+ '</b>' + ': ' + this.y + '<br/>';
 
             },
 
@@ -73,13 +74,13 @@ $(document).ready(function () {
                 name: 'Deutsche',
                 data: [3, 5, 75, 116, 120, 120, 73, 70, 60, 53, 25, 23, 23, 18, 10, 8, 6, 2, 1],
                 stack: 'deutsch',
-                color: '#113841'
+                color: "#b9c4c6"
             },
             {
                 name: 'Nicht-Deutsche',
                 data: [4, 18, 61, 90, 130, 129, 102, 89, 50, 57, 30, 21, 10, 9, 3, 1, 4, 2, 1],
                 stack: 'nichtdeutsch',
-                color: '#4aa6c0'
+                color: '#68c3d8'
             }]
     });
 });
